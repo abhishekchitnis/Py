13cd8695f5520124366dc61e39417ef602366f09
Abhishek Chitnis <abhishek210022@gmail.com>
2018-11-21 18:18:01 +0530

    Update Check.py

--- PyDump/Check.py
+++ PyDump/Check.py
@@ -1,14 +1,22 @@
-# Python program to demonstrate working of 
-# getpass.getuser() 
-import getpass 
+import pyautogui
 
-user = getpass.getuser() 
+print(pyautogui.position())
 
-while True: 
-	pwd = getpass.getpass("User Name : {0}".format(user)+"\nPassword : ") 
+pyautogui.click(131, 751, interval=0.25)
+pyautogui.typewrite('Paint', interval=0.1)
+pyautogui.click(127, 208, interval=1.25)
 
-	if pwd == 'abcd': 
-		print ("Welcome!!!")
-		break
-	else: 
-		print ("The password you entered is incorrect.")
+import pyautogui
+x, y = pyautogui.locateCenterOnScreen('ic.png', grayscale=True)
+print(x, y)
+pyautogui.click(x, y)
+pyautogui.mouseDown(button='left', x=207, y=608)
+pyautogui.mouseDown(button='left', x=467, y=283)
+pyautogui.mouseDown(button='left', x=849, y=593)
+pyautogui.mouseUp(button='left', x=207, y=608)
+
+pyautogui.alert(text='Triangle Drawn, Type Try in TextBox to Continue...', title='Triangle Drawn', button='OK')
+pyautogui.password(text='Triangle Drawn, Type Try in TextBox to Continue...', title='Triangle Drawn', default='', mask='*')
+
+# https://pyautogui.readthedocs.io/en/latest/mouse.html
+# https://pyautogui.readthedocs.io/en/latest/keyboard.html
