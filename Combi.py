c62b8442557c3fbce4728bc3049e0f873e99cabd
Abhishek Chitnis <abhishek210022@gmail.com>
2019-01-09 15:56:04 +0530

    Update Combi.py

--- Combi.py
+++ Combi.py
@@ -4,29 +4,40 @@ import requests
 import pyautogui
 from pathlib import Path
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 user = getpass.getuser()
-file = "C:\\Users\\{0}\\Downloads\\chromedriver.exe".format(user)
-filechk = Path(file)
+chrdrv = "C:\\Users\\{0}\\Downloads\\chromedriver.exe".format(user)
 url = "http://www.muhurtnews.com/Abhishek/chromedriver.exe"
-if filechk.is_file():
+if Path(chrdrv).is_file():
 	print()
 else:
     r = requests.get(url, stream = True)
     with open(file, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             f.write(chunk)
+			
+print("Enter Last 4 Digits of SAP ID :") 
+sid=input()
 
-driver = webdriver.Chrome(file)
-driver.maximize_window()
+opts = Options()
+opts.add_argument('start-maximized')
+opts.add_argument('disable-infobars')
+driver = webdriver.Chrome(options=opts, executable_path=chrdrv)
+#driver.maximize_window()
 driver.get("http://10.135.26.21:8079/siteforge/jsp/index.jsp")
-driver.find_element_by_xpath("//*[@class='form-control username']").send_keys('mumbai.cmm')
+driver.find_element_by_xpath("//*[@class='form-control username']").send_keys('mumbai.cml')
 driver.find_element_by_xpath('//*[@value="Submit"]').click()
 time.sleep(0.5)
 driver.find_element_by_xpath("//*[@name='j_password']").send_keys('Pass_123')
 driver.find_element_by_xpath('//*[@id="Login_button"]').click()
-driver.find_element_by_id('rfcSiteSearchTab').send_keys('9496')
-driver.find_element_by_xpath('//*[@id="fillterBySiteNameORSapIdDiv"]').click()
+driver.find_element_by_id('rfcSiteSearchTab').send_keys(sid)
+driver.find_element_by_xpath('//*[@id="fillterBySiteNameORSapIdDiv"]/button').click()
+time.sleep(0.5)
+driver.find_element_by_xpath('//*[@id="rfc_site_list_table"]/tbody/tr[2]/td[8]/a').click()
+time.sleep(0.5)
+driver.find_element_by_xpath('//*[@id="siteCategory"]').click()
+
 
 '''
 IMPORTANT NOTEs
