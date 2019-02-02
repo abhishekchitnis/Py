4e9a711eee985059a3758cf8428caffb67766539
abhishekchitnis <abhishek210022@gmail.com>
2019-02-02 22:34:42 +0530

    AutoDrawPaint
    Painter

--- PyAutoPaint.py
+++ PyAutoPaint.py
@@ -8,7 +8,7 @@ pyautogui.click(148, 767, interval=0.50)
 pyautogui.typewrite('Paint', interval=0.05)
 time.sleep(2)
 pyautogui.click(157, 263, interval=1.50)
-
+time.sleep(2)
 import pyautogui
 x, y = pyautogui.locateCenterOnScreen('ic.png', grayscale=True)
 print(x, y)
@@ -19,7 +19,9 @@ pyautogui.mouseDown(button='left', x=849, y=593)
 pyautogui.mouseUp(button='left', x=207, y=608)
 
 pyautogui.alert(text='Triangle Drawn, Type Try in TextBox to Continue...', title='Triangle Drawn', button='OK')
-pyautogui.password(text='Triangle Drawn, Type in TextBox to Continue...', title='Triangle Drawn', default='', mask='*')
+pwd=pyautogui.password(text='Triangle Drawn, Type in TextBox to Continue...', title='Triangle Drawn', default='', mask='*')
+
+print(pwd)
 
 # https://pyautogui.readthedocs.io/en/latest/mouse.html
 # https://pyautogui.readthedocs.io/en/latest/keyboard.html
