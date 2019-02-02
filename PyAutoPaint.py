36cd351b07efe67dfd423f4d857554b8d7a1a3b8
abhishekchitnis <abhishek210022@gmail.com>
2019-02-03 03:16:17 +0530

    Update PyAutoPaint.py

--- PyAutoPaint.py
+++ PyAutoPaint.py
@@ -7,12 +7,14 @@ pyautogui.moveTo(148, 767,0.5)
 pyautogui.click(148, 767, interval=0.50)
 pyautogui.typewrite('Paint', interval=0.05)
 time.sleep(2)
+
 pyautogui.click(157, 263, interval=1.50)
 time.sleep(2)
-import pyautogui
+
 x, y = pyautogui.locateCenterOnScreen('ic.png', grayscale=True)
 print(x, y)
 pyautogui.click(x, y)
+
 pyautogui.mouseDown(button='left', x=207, y=608)
 pyautogui.mouseDown(button='left', x=467, y=283)
 pyautogui.mouseDown(button='left', x=849, y=593)
