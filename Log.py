4b5753df0d84f7779ec8422cc1131d60b6495b56
Abhishek Chitnis <abhishek210022@gmail.com>
2021-05-06 03:09:07 +0530

    Update Log.py

--- Log.py
+++ Log.py
@@ -4,3 +4,20 @@ logging.debug('This message should go to the log file')
 logging.info('So should this')
 logging.warning('%s before you %s', 'Look', 'leap!')
 logging.error('And non-ASCII stuff, too, like Err0r')
+
+'''
+Levels = When they're used
+
+DEBUG = Detailed information, typically of interest only when diagnosing problems.
+
+INFO = Confirmation that things are working as expected.
+
+WARNING = An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+
+ERROR = Due to a more serious problem, the software has not been able to perform some function.
+
+CRITICAL = A serious error, indicating that the program itself may be unable to continue running.
+
+More Info : https://docs.python.org/3/howto/logging.html
+
+'''
