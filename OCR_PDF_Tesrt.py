031cdcbe368dfcf41deb3b53d16b7f5d3dec4c5b
Abhishek Chitnis <abhishek210022@gmail.com>
2023-05-29 11:47:19 +0530

    Create OCR_PDF_Tesrt.py

---
+++ OCR_PDF_Tesrt.py
@@ -0,0 +1,26 @@
+import fitz
+pdf = fitz.open('pdf.pdf')
+page = pdf.load_page(0)
+pix = page.get_pixmap() #(dpi=300)
+pix.pil_save("img2.jpg") 
+
+
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# Grayscale, Gaussian blur, Otsu's threshold
+image = cv2.imread('img2.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (3,3), 0)
+thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Morph open to remove noise and invert image
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+invert = 255 - opening
+
+# Perform text extraction
+data = pytesseract.image_to_string(invert, lang='eng', config='--psm 6')
+print(data)
